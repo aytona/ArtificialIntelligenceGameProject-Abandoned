8c59532dbc383559f3bfd14d7551bcdd1f472b3e
aytona <christopher.aytona@gmail.com>
2015-11-26 10:36:28 -0500

    Unity Project Commit

---
+++ Assets/Scripts/MISC/DefaultBullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DefaultBullet : MonoBehaviour {
+
+    public float speed = 5f;
+    public Vector3 myDirection = Vector3.left;
+    public float lifeSpan = 5f;
+    
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(LifespanCoroutine());
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(myDirection * speed);
+    }
+
+    private IEnumerator LifespanCoroutine()
+    {
+        yield return new WaitForSeconds(this.lifeSpan);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject defaultBullet;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(this.defaultBullet) as GameObject;
+            bullet.transform.position = this.transform.position;
+            bullet.transform.rotation = this.transform.rotation;
+        }
+    }
+}
