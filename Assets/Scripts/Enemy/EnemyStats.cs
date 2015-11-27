36ad2a6f768a57f4f35d7379fbe69a1d41c24a60
Christopher Aytona <christopher.aytona@hotmail.com>
2015-11-27 13:46:53 -0500

    Enemy stats: health

---
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStats : MonoBehaviour {
+
+    public enum EnemyTypes
+    {
+        MELEE = 35,
+        RANGE = 15,
+        BOSS = 100
+    }
+
+    private EnemyTypes type;
+    private int health;
+
+    void Start()
+    {
+        CheckType();
+        Debug.Log(health);
+    }
+
+    private void CheckType()
+    {
+        if (gameObject.tag == "Melee")
+            type = EnemyTypes.MELEE;
+        if (gameObject.tag == "Range")
+            type = EnemyTypes.RANGE;
+        if (gameObject.tag == "Boss")
+            type = EnemyTypes.BOSS;
+        health = (int)type;
+    }
+}
