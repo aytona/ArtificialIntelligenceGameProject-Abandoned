6be72a39fa32f79b6a7bdb1ba97235aebb051c93
aytona <christopher.aytona@gmail.com>
2015-11-30 23:25:38 -0500

    Initial castlestats script

---
+++ Assets/Scripts/Player/CastleStats.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CastleStats : MonoBehaviour {
+
+    public enum CastleTypes
+    {
+        LevelOne = 50,
+        LevelTwo = 80,
+        LevelThree = 120,
+        LevelFour = 150,
+        LevelFive = 200
+    }
+    public CastleTypes upgrade;
+
+    private CastleTypes current;
+    private int health;
+
+    void Start()
+    {
+        // Initial health
+        health = (int)current;
+    }
+
+    void Update()
+    {
+        SetType();
+
+        // If the current type does not equal to the updated(upgrade) type
+        if (current != upgrade)
+        {
+            // Upgrade health and change current type to the new upgrade type
+            health = (int)upgrade;
+            current = upgrade;
+        }
+    }
+
+    private void SetType()
+    {
+        // Need someway to upgrade castle and change type through UI
+    }
+}
