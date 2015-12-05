f9524a6ba8f94c958e82de123e749c3ceea82d60
aytona <christopher.aytona@gmail.com>
2015-12-05 18:47:04 -0500

    Revert "enemy attack add"
    This reverts commit 449d0d4b64361a4d074298bb5607b0cceee1e77d.

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -9,17 +9,51 @@ public class EnemyStats : MonoBehaviour {
         RANGE = 15,
         BOSS = 100
     }
+    public GameObject attackObject;
 
     private EnemyTypes type;
     private int health;
+    private int bulletDamage = 5;
+    private int shotgunDamage = 10;
+    private int lazerDamage = 3;
 
     void Start()
     {
-        CheckType();
-        Debug.Log(health);
+        SetType();
     }
 
-    private void CheckType()
+    void Update()
+    {
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Bullet")
+        {
+            health -= bulletDamage;
+            Destroy(other.gameObject);
+        }
+        if (gameObject.tag == type.ToString() + " Attack")
+            attackObject.SetActive(true);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (gameObject.tag == "Lazer")
+            health -= lazerDamage;
+        if (gameObject.tag == "Shotgun")
+            health -= shotgunDamage;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (gameObject.tag == type.ToString() + " Attack")
+            attackObject.SetActive(false);
+    }
+
+    private void SetType()
     {
         if (gameObject.tag == "Melee")
             type = EnemyTypes.MELEE;
